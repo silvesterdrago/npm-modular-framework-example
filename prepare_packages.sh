006a62299d3165c551d660d576f45a6bd7078975
Denis Epstein <denisepstein1337@gmail.com>
2024-11-12 11:16:03 +0200

    upd prepare_packages script

--- prepare_packages.sh
+++ prepare_packages.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#
 
-cd super_greeter_browser_provider
-npm i && npm run build && npm run publish_local
-cd ../super_greeter_server_provider
-npm i && npm run build && npm run publish_local
-cd ../super_greeter
-npm i && npm run build && npm run publish_local
-cd ..
+prepare_package() {
+    package_path=$1
+    cd "$package_path" || exit
+    npm i && npm run build && npm run publish_local
+    cd ..
+}
+
+prepare_package "super_greeter_browser_provider"
+prepare_package "super_greeter_server_provider"
+prepare_package "super_greeter"
